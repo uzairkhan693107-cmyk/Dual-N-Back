057830a2ebd44f953bb99890d0ccbd622067ef50
uzairkhan693107-cmyk <uzairkhan693107@gmail.com>
2026-02-10 12:08:26 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'n-back-v2';
+const ASSETS = [
+  './index.html',
+  './js/chart.js',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
